a104c84fd049d48d80e7c6f06ecc3799b0e49160
Shivam Gupta <74005091+coderS176@users.noreply.github.com>
2024-03-14 00:15:38 +0530

    Update leetcode.py

--- script/leetcode.py
+++ script/leetcode.py
@@ -2,6 +2,8 @@ import time
 from bs4 import BeautifulSoup
 import re
 
+# def get_rating()
+
 def get_problems_solved(driver, username):
     url = f'https://leetcode.com/{username}'
     problems_leetcode = []
@@ -12,6 +14,7 @@ def get_problems_solved(driver, username):
 
     # Extract the titles of the most recent problems solved
     div = soup.find_all('div', attrs={'data-title': True})
+    rating = soup.find
     for i in div:
         children = i.findChildren('span')
         pattern = r'^(a\ minute\ ago|a\ few\ seconds\ ago|an\ hour\ ago|\d+\ hours\ ago|\d+\ minutes\ ago)$'
@@ -21,7 +24,7 @@ def get_problems_solved(driver, username):
             submission_link = f"https://leetcode.com{i.parent['href']}"
             problem_link = f"https://leetcode.com/problems/{i['data-title'].lower().replace(' ', '-')}/"
             submission_id = i.parent['href'].split('/')[-2]
-            problems_leetcode.append({ 'problem_title':  i['data-title'], 'problem_link': problem_link, 'submission_link': submission_link, 'submission_id':submission_id, 'platform': 'Leetcode', 'username': username })
+            problems_leetcode.append({ 'problem_title':  i['data-title'], 'problem_link': problem_link, 'submission_link': submission_link, 'submission_id':submission_id, 'platform': 'Leetcode', 'username': username, 'current_rating': rating })
         else:
             break
 
